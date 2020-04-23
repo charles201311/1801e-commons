03c2dd1c60638ec109d71e25c280c4ba30d489aa
yangchunbo <348828809@qq.com>
2020-04-23 12:00:48 +0800

    工具类第一天，随机数工具类

---
+++ src/main/java/com/bobo/common/utils/StringUtil.java
@@ -0,0 +1,11 @@
+package com.bobo.common.utils;
+/**
+ * 
+ * @ClassName: StringUtil 
+ * @Description: 字符串处理工具类
+ * @author: charles
+ * @date: 2020年4月23日 上午10:40:06
+ */
+public class StringUtil {
+
+}
