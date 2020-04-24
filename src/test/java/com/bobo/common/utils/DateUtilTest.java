369aba8abd27b1a76146a9b86b27a5eed392cc2c
yangchunbo <348828809@qq.com>
2020-04-24 16:52:08 +0800

    fileUti   StreamUtil

--- src/test/java/com/bobo/common/utils/DateUtilTest.java
+++ src/test/java/com/bobo/common/utils/DateUtilTest.java
@@ -8,6 +8,32 @@ import org.junit.Test;
 
 public class DateUtilTest {
 	
+	
+	@Test
+	public void testSql() {
+		/**
+		 * 将上面{1}的位置使用DateUtil工具类中的getDateByInitMonth()返回值替换
+		 * ，将上面{2}的位置使用DateUtil工具类中的getDateByFullMonth()返回值替换
+		 * 。最后打印出正确拼接的SQL字符串。
+		 */
+		
+		
+		
+		String sql = "select * from t_order where create_time>='{1}' and create_time<='{2}' ";
+		
+		Date initDate = DateUtil.getInitMonth(new Date());
+		Date endMonth = DateUtil.getEndMonth(new Date());
+		SimpleDateFormat fm = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String date1 = fm.format(initDate);
+		String date2 = fm.format(endMonth);
+		
+		String newSql = sql.replace("{1}", date1).replace("{2}", date2);
+		
+		System.out.println(newSql);
+		
+		
+	}
+	
 
 	@Test//根据日期计算年龄
 	public void testGetAge() {
